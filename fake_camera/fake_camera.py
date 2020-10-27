05686f729f4514f4e7cf339de9b8b6f794c2fe44
Indrit Fejza <fejza.indrit@gmail.com>
2020-10-27 01:10:08 +0100

    Updated everything

--- fake_camera/fake_camera.py
+++ fake_camera/fake_camera.py
@@ -4,32 +4,68 @@ import copy
 
 class Fake_Camera():                                      
    
-    def __init__(self, image = 'lena_color.jpg', black_white = 1, size = (1000, 1000), add_noise = 1):  
+    def __init__(self, image = 'lena_color.jpg', colour = "c", background_colour = "b", 
+                       size = (1000, 1000), add_noise = 0, pixel_move = 5):  
+        """
+        Library for creating a moving image on the screen.
         
-        self.image            = image
-        self.black_white      = black_white
-        self.canvas_size      = size
-        self.add_noise        = add_noise
-        self.flip_sign        = 0
-        self.pixel_move       = 5
-        self.main_config()
-
-    def main_config(self):
+        args:
+            image             = ".jpg"        Image to be displayed
+            colour            = "c" or "g"    for colour or grayscale
+            backgroung_colour = "w", "b", "r" for white, black, random
+            size              = (1000,1000)   Size of the Canvas
+            add_noise         = "Yes", "No"   Add noise to the images
+            pixel_move        = int()         How much pixels the image moves
+        """
         
-        self.canvas           = np.random.randint(1, 255, size = self.canvas_size, dtype='uint8')  
-        try:
-            self.frame = np.asarray ( Image.open("lena_color.jpg").convert('L') )
-
-        except Exception:
-            self.frame        = np.random.randint(1, 255, size = (int(self.canvas_size[0]/2), int(self.canvas_size[1]/2) ), dtype='uint8')  
-        
-        if  self.black_white == 1:
-            self.config_black_white()
-  
+        self.image             = image
+        self.colour            = colour
+        self.background_colour = background_colour
+        self.canvas_size       = size
+        self.add_noise         = add_noise
+        self.pixel_move        = pixel_move
+        self.flip_sign         = 0
+        self.image_config()
+        self.background_config()
+        self.canvas_config()
         if self.add_noise:       
             self.config_noise()
-             
-    def config_black_white(self):
+
+    def image_config(self):
+        if  self.colour == "g":
+            try:
+                self.frame = np.asarray ( Image.open(self.image).convert('L') )
+            except Exception:
+                    print("Image does not exist in the current directory!")
+                    return
+            
+        elif  self.colour == "c":
+            try:
+                self.frame = np.asarray ( Image.open(self.image) )
+                self.frame = self.frame[...,[2,1,0]]
+            except Exception:
+                    print("Image does not exist in the current directory!")
+                    return
+
+    def background_config(self):
+               
+        if  self.colour == "g":
+            if  self.background_colour == "r":
+                self.canvas           = np.random.randint(1, 255, size = self.canvas_size, dtype='uint8') 
+            if  self.background_colour == "w":
+                self.canvas           = np.ones(self.canvas_size, dtype='uint8') * 255
+            if  self.background_colour == "b":
+                self.canvas           = np.zeros(self.canvas_size, dtype='uint8') 
+        
+        if  self.colour == "c":
+            if  self.background_colour == "r":
+                self.canvas           = np.random.randint(1, 255, size = self.canvas_size + (3,), dtype='uint8') 
+            if  self.background_colour == "w":
+                self.canvas           = np.ones(self.canvas_size + (3,), dtype='uint8') * 255
+            if  self.background_colour == "b":
+                self.canvas           = np.zeros(self.canvas_size + (3,), dtype='uint8')             
+                        
+    def canvas_config(self):
         
         self.upper_left_point_x = int( self.canvas.shape[0] / 4 )
         self.upper_left_point_y = int( self.canvas.shape[0] / 4 )
@@ -44,18 +80,25 @@ class Fake_Camera():
         
         self.canvas_view   = self.canvas[ self.limit_x_start : self.limit_x_end, self.limit_y_start : self.limit_y_end ]        
         self.old_start_x   = self.upper_left_point_x
-        self.old_start_y   = self.upper_left_point_y
+        self.old_start_y   = self.upper_left_point_y     
         
     def config_noise(self):
-        self.noise_start   = np.zeros( ( self.canvas_view.shape[0],self.canvas_view.shape[1] ), np.uint8 )
         self.mean          = 0
         self.var           = 0.1
         self.sigma         = self.var **0.5  
         
-    def add_noise_to_image(self, image):
-        row, col    = image.shape
-        self.gauss  = np.random.normal( self.mean, self.sigma, (row, col) ) * 50
-        self.gauss  = self.gauss.reshape( row, col )
+    def add_noise_to_image(self, image ):
+        
+        if len(image.shape) == 2:
+            row, col    = image.shape
+            self.gauss  = np.random.normal( self.mean, self.sigma, (row, col) ) * 50
+            self.gauss  = self.gauss.reshape( row, col )
+        
+        elif len(image.shape) == 3:
+            row, col, ch    = image.shape
+            self.gauss  = np.random.normal( self.mean, self.sigma, (row, col, ch) ) * 50
+            self.gauss  = self.gauss.reshape( row, col, ch )
+        
         self.gauss  = self.gauss.astype("uint8")
         noisy_image = image + self.gauss
         return noisy_image
@@ -94,4 +137,4 @@ class Fake_Camera():
             
         except Exception:
             print("Error Somewhere")
-#---------------------------------------------------------------------------------------------------------------------------#   
+#-----------------------------------------------------------------------------#
